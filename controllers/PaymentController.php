3631b2f4ed0d2edf67adf69d278f4d2618b06b81
Dominik Pfaffenbauer <dominik@lineofcode.at>
2016-01-27 14:24:29 +0100

    use new Configuration Manager

--- controllers/PaymentController.php
+++ controllers/PaymentController.php
@@ -25,7 +25,7 @@ class CoreShopBankwire_PaymentController extends Payment
     public function paymentAction()
     {
         //DoPayment
-        $this->session->order = $this->getModule()->createOrder($this->cart, \CoreShop\Model\OrderState::getById(\CoreShop\Config::getValue("ORDERSTATE.BANKWIRE")), $this->cart->getTotal(), $this->view->language);
+        $this->session->order = $this->getModule()->createOrder($this->cart, \CoreShop\Model\OrderState::getById(\CoreShop\Model\Configuration::get("SYSTEM.ORDERSTATE.BANKWIRE")), $this->cart->getTotal(), $this->view->language);
 
         $this->redirect($this->getModule()->getConfirmationUrl());
     }
