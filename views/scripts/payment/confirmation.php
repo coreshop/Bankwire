99851dc59db820194f11edb04ba37b6d9817495b
dpfaffenbauer <dominik@lineofcode.at>
2016-05-25 13:34:42 +0200

    confirmationAction

--- views/scripts/payment/confirmation.php
+++ views/scripts/payment/confirmation.php
@@ -1,10 +1,6 @@
-<div class="container shop checkout checkout-step-5">
-    <?=$this->partial("coreshop/helper/order-steps.php", array("step" => 5));?>
-
-    <p>
-        <?php
+<p>
+    <?php
         echo sprintf($this->translate("Your Order has been submitted. You have choosen to by with Bankwire.<br/>Your Order Number is %s, please follow the instructions:"), $this->order->getOrderNumber())
-        ?>
-    </p>
-    <p><?php echo html_entity_decode(\CoreShop\Model\Configuration::get("BANKWIRE.TEXT." . strtoupper($this->language)))?></p>
-</div>
+    ?>
+</p>
+<p><?php echo html_entity_decode(\CoreShop\Model\Configuration::get("BANKWIRE.TEXT." . strtoupper($this->language)))?></p>
